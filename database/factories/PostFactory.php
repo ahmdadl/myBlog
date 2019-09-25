54faf2805768d3259495c8595a5076570bba2bee
ahmed adel <abo3adel35@gmail.com>
2019-09-25 21:07:28 +0200

    add default images for posts

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(3),
+        'body' => $faker->text(350),
+        'img' => $faker->randomKey([1, 2, 3, 4, 5]) . '.png',
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        }
+    ];
+});
