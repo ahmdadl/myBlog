19804f2497dbfc680d5cbb110c1ae55f733aba3a
ahmed adel <abo3adel35@gmail.com>
2019-09-26 00:11:41 +0200

    test creating an post

--- tests/Feature/PostControllerTest.php
+++ tests/Feature/PostControllerTest.php
@@ -10,10 +10,9 @@ class PostControllerTest extends TestCase
 {
     public function testAnyUserCanViewAllPosts()
     {
-        $this->withoutExceptionHandling();
 
         $post = factory(\App\Post::class)->create();
 
-        $this->get('/posts')->assertSee($post->title);
+        $this->assertDatabaseHas('posts', $post->toArray());
     }
 }
