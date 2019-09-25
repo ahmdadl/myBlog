a3000915e5fc2808389bfeaef9d98004b4c98f6a
ahmed adel <abo3adel35@gmail.com>
2019-09-25 21:26:52 +0200

    add no posts alert

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="row">
-        @foreach ($posts as $post)
+        @forelse ($posts as $post)
             <div class='card col-sm-6 col-md-4 col-lg-3'>
                 <div class="card border-primary">
                   <img class="card-img-top" src="{{asset('img/'.$post->img)}}" alt="{{$post->title}}">
@@ -16,6 +16,10 @@
                   </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <div class="alert alert-warning m-auto" style="width:50%">
+                <b>Note:</b> no posts has been added yet
+            </div>
+        @endforelse
     </div>
 @endsection
