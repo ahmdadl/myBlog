5760be41c6cf77ea444a43328ffa780e82953527
ahmed adel <abo3adel35@gmail.com>
2019-09-28 16:03:58 +0200

    seed users with posts

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Post;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create admin account
+        factory(User::class)->create([
+            'email' => 'admin@example.tld',
+            'perm' => 31
+        ]);
+
+        //create super account
+        factory(User::class)->create([
+            'email' => 'super@example.tld',
+            'perm' => 15
+        ]);
+
+        // create random users with posts
+        factory(User::class, 20)->create([
+            'perm' => Arr::random([1, 2, 4, 8, 16])
+        ])->each(function ($user) {
+            $user->posts()->save(
+                factory(Post::class)->make()
+            );
+        });
+    }
+}
