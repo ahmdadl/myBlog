43a3ad673ad3fe583c3591c32ee8d7aaa019b097
ahmed adel <abo3adel35@gmail.com>
2019-10-09 19:29:15 +0200

    styling create category form

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -5,15 +5,17 @@
 @endsection
 
 @section('content')
-    <fieldset class='bg-dark text-light'>
-        <legend class="text-info">Create New Category</legend>
-        <form class="form form-horizontal" action="category" method="POST">
-            @csrf
-            <div class="form-group">
-              <label for="title">Title</label>
-              <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="CategoryTitle">
-              <small id="helpId" class="form-text text-muted">must be less than 20 characters</small>
-            </div>
-        </form>
-    </fieldset>
+    <div class="p-2 bg-dark text-light">
+            <fieldset class=' p-2'>
+                    <legend class="text-warning">Create New Category</legend>
+                    <form class="form form-horizontal" action="category" method="POST">
+                        @csrf
+                        <div class="form-group">
+                          <label for="title">Title</label>
+                          <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="CategoryTitle">
+                          <small id="helpId" class="form-text text-muted">must be less than 20 characters</small>
+                        </div>
+                    </form>
+                </fieldset>
+    </div>
 @endsection
