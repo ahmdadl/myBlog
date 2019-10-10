4217bf65dc45d237fe17a071ea40aad5962ae76c
ahmed adel <abo3adel35@gmail.com>
2019-10-10 12:45:00 +0200

    styling comment list

--- resources/views/post/comment/index.blade.php
+++ resources/views/post/comment/index.blade.php
@@ -1,3 +1,14 @@
-@foreach ($post->comments as $comment)
-    {{$comment->body}}<br>
-@endforeach
+<ul class="list-unstyled">
+    @foreach ($post->comments as $comment)
+    <li class="media {{ $loop->last ?: 'mb-4' }}">
+        <img src="{{asset('img/user.png')}}" class="mr-3"
+            alt="{{$comment->owner->name}}">
+        <div class="media-body">
+            <h5 class="mt-0 mb-1">
+                {{$comment->owner->name}}
+            </h5>
+            {{$comment->body}}
+        </div>
+    </li>
+    @endforeach
+</ul>
