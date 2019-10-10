ed4cc6e07a8b90a308d82e2bae837919d0a6d9d5
ahmed adel <abo3adel35@gmail.com>
2019-10-10 13:00:40 +0200

    create post comment form

---
+++ resources/views/post/comment/form.blade.php
@@ -0,0 +1,15 @@
+<div class="bg-dark text-light">
+    <fieldset class="p-2">
+        <legend class="text-primary">Create New Comment</legend>
+        <form action="{{$post->path() . '/comments'}}" method="POST" class="form">
+            @csrf
+            <div class="form-group">
+              <label for="body">Comment</label>
+              <textarea class="form-control" name="body" id="body" rows="4" placeholder="Write Your Comment"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success btn-block">Comment</button>
+            </div>
+        </form>
+    </fieldset>
+</div>
