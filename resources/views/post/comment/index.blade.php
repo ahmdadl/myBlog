7aabccc41eada356bc4055d98a1d2f34054ababc
ahmed adel <abo3adel35@gmail.com>
2019-10-10 13:01:01 +0200

    fix user image size

--- resources/views/post/comment/index.blade.php
+++ resources/views/post/comment/index.blade.php
@@ -1,14 +1,17 @@
-<ul class="list-unstyled">
-    @foreach ($post->comments as $comment)
-    <li class="media {{ $loop->last ?: 'mb-4' }}">
-        <img src="{{asset('img/user.png')}}" class="mr-3"
-            alt="{{$comment->owner->name}}">
-        <div class="media-body">
-            <h5 class="mt-0 mb-1">
-                {{$comment->owner->name}}
-            </h5>
-            {{$comment->body}}
-        </div>
-    </li>
-    @endforeach
-</ul>
+<div class="col-12 mt-4">
+    @include('post.comment.form')
+    <ul class="list-unstyled mt-3">
+        @foreach ($post->comments as $comment)
+        <li class="media {{ $loop->last ?: 'mb-4' }}">
+            <img src="{{asset('img/user.png')}}" class="mr-3 rounded"
+                alt="{{$comment->owner->name}}" width="150" height="100">
+            <div class="media-body">
+                <h5 class="mt-0 mb-1 text-primary">
+                    {{$comment->owner->name}}
+                </h5>
+                {{$comment->body}}
+            </div>
+        </li>
+        @endforeach
+    </ul>
+</div>
