9a0a7dc6d0741a1e1aff2b0bb9b3dc127f326658
ahmed adel <abo3adel35@gmail.com>
2019-10-10 13:58:03 +0200

    create post in comment factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,12 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Comment;
+use Facades\Tests\Setup\PostFactory;
 use Facades\Tests\Setup\UserFactory;
 use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
     return [
         'userId' => UserFactory::create(),
+        'postId' => PostFactory::create(),
         'body' => $faker->text
     ];
 });
