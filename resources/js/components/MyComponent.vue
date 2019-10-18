04a9034838169fe75b694fae73d1df5101145240
ahmed adel <abo3adel35@gmail.com>
2019-10-18 16:57:17 +0200

    it still worjing

---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="onClick">Click!</button>
+</template>
+
+<script lang="ts">
+    import * as Vue from "vue";
+    import Component from "vue-class-component";
+
+    @Component
+    export default class MyComponent extends Base {
+        // Initial data can be declared as instance properties
+        message: string = "Hello!";
+
+        // Component methods can be declared as instance methods
+        onClick(): void {
+            window.alert(this.message);
+            this.$el.textContent = "some of Me";
+        }
+    }
+</script>
