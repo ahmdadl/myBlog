33015a9a3975b15e8f0f0b128a212f487e64a229
ahmed adel <abo3adel35@gmail.com>
2019-12-29 13:20:51 +0200

    change footer to my site

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -39,8 +39,7 @@
     <!-- Footer -->
     <footer class="py-5 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your
-                Website 2019</p>
+            <p class="m-0 text-center text-white">Made with &hearts; by <a href="http://ninjacoder.rf.gd/">ninjaCoder</a> 2019</p>
         </div>
         <!-- /.container -->
     </footer>
